0b08f92ef44f7953345d869dfadf984e4dbd8f43
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2023-06-28 18:53:54 +0530

    Add files via upload

---
+++ LeetCode/344ReverseString.cpp
@@ -0,0 +1,20 @@
+#include<string>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+
+
+        int st=0;
+        int end=s.size()-1;
+
+        while(st<=end)
+        {
+            swap(s[st++],s[end--]);
+        }
+        
+    }
+};
