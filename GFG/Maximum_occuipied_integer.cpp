d7224fb5eee8c25a0d4d4b91b628b29c230c56ca
Adivya Jain <adivyajain2003@gmail.com>
2024-06-08 14:09:20 +0530

    Replace Words || Maximum occupied Integer

---
+++ GFG/Maximum_occuipied_integer.cpp
@@ -0,0 +1,86 @@
+// Maximum occured integer
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // l and r are input array
+    // maxx : maximum in r[]
+    // n: size of array
+    // arr[] : declared globally with size equal to maximum in l[] and r[]
+    // Function to find the maximum occurred integer in all ranges.
+    int maxOccured(int n, int l[], int r[], int maxx) {
+
+        // Your code here
+        map<int,int> mp;
+        
+        for(int i=0;i<n;i++)
+        {
+            int ele = l[i];
+            
+            while(ele<=r[i])
+            {
+                mp[ele]++;
+                ele++;
+            }
+        }
+        int max_element;
+        int max_count = -1;
+        for (const auto& pair : mp) {
+            if (pair.second > max_count) {
+                max_count = pair.second;
+                max_element = pair.first;
+        }
+    }
+    
+    return max_element;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    // taking testcases
+    cin >> t;
+
+    while (t--) {
+        int n;
+
+        // taking size of array
+        cin >> n;
+        int l[n];
+        int r[n];
+
+        // adding elements to array L
+        for (int i = 0; i < n; i++) {
+            cin >> l[i];
+        }
+
+        int maxx = 0;
+
+        // adding elements to array R
+        for (int i = 0; i < n; i++) {
+
+            cin >> r[i];
+            if (r[i] > maxx) {
+                maxx = r[i];
+            }
+        }
+        Solution ob;
+
+        // calling maxOccured() function
+        cout << ob.maxOccured(n, l, r, maxx) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
