083b8097b1342e09f2fe3bc0ebd945e38c235e0e
Adivya Jain <adivyajain2003@gmail.com>
2024-08-17 01:25:05 +0530

    GREEDY

--- LeetCode/624_maximumDistanceInArray.cpp
+++ LeetCode/624_maximumDistanceInArray.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int start = pow(10,4);
-        int end = - pow(10,4);
-
-        for (int i = 0; i < arrays.size(); i++) {
-            int currSize = arrays[i].size();
-            if(abs(start - arrays[i][0]) > abs(end - arrays[i][currSize-1]))
-            {
-                start = min(start, arrays[i][0]);
-            }
-            else{
-                end = max(end, arrays[i][currSize - 1]);
-            }
+        int MIN = arrays[0].front();
+        int MAX = arrays[0].back();
+        int result = 0;
+        for (int i = 1; i < arrays.size(); i++) {
+            int currStart = arrays[i].front();
+            int currEnd = arrays[i].back();
+            result = max({result,abs(currStart - MAX),abs(currEnd - MIN)});
+            MAX = max(MAX,currEnd);
+            MIN = min(MIN,currStart);
         }
 
-        return end - start;
+        return result;
     }
 };
