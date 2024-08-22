63a5bd8a8d4c46f5b0b48eca16e98f6404d5da7f
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2024-08-22 23:59:28 +0530

    BIT MANIPULATION

---
+++ LeetCode/476_complementNumber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int ans;
+        int i=0;
+        while(num)
+        {
+            ans += (num && 1)*pow(2,i);
+            i++;
+            num = num>>1;
+        }
+        return ans;
+    }
+};
