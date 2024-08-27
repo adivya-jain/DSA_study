86d60aa2d57d915b7d0373dde04683e20432bc50
Adivya Jain <adivyajain2003@gmail.com>
2024-08-27 22:42:41 +0530

    RECURSION

--- LeetCode/509_Fibonacci_recursion.cpp
+++ LeetCode/509_Fibonacci_recursion.cpp
@@ -20,3 +20,23 @@ public:
         
     }
 };
+//
+class Solution {
+    int solve(int n, int n1, int n2) 
+    {
+        if(n==0) return n1+n2;
+        return solve(n-1,n2,n1+n2);
+    }
+
+public:
+    int fib(int n) {
+        int n1 = 0;
+        int n2 = 1;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int ans = solve(n - 2, n1, n2);
+        return ans;
+    }
+};
