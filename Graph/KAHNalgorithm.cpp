cb6e8d0a8e1beb06efb0cff1f5a190fdd80555c9
Adivya Jain <adivyajain2003@gmail.com>
2024-09-04 03:15:43 +0530

    KAHN'S ALGORITHM TOPOLOGICAL SORT

---
+++ Graph/KAHNalgorithm.cpp
@@ -0,0 +1,46 @@
+// Kahn's Algorithm for Topological Sorting
+// Time Complexity: O(V+E)
+// Space Complexity: O(V+E)
+
+#include <bits/stdc++.h>
+
+void toposort(int i, unordered_map<int,vector<int>>& adj,vector<bool>&visited,stack<int>&st)
+{
+    visited[i]=true;
+    for(auto it:adj[i])
+    {   
+        if(!visited[it])
+        {
+            toposort(it, adj, visited, st);
+        }
+    }
+    st.push(i);
+
+}
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    unordered_map<int,vector<int>> adj;
+    for(int i=0;i<e;i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+    }
+
+    vector<bool> visited(v);
+    stack<int> st;
+    for(int i =0;i<v;i++)
+    {
+       if (!visited[i]) { 
+            toposort(i, adj, visited, st);
+        }
+    }
+    
+    vector<int> ans;
+    while(!st.empty())
+    {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
