e3198ab46aa5647dcd86dd7f34399ffa3d7a2cc5
Adivya Jain <95540661+adivya-jain@users.noreply.github.com>
2025-02-17 16:46:27 +0530

    Update 141_linked_list_cycle.cpp

--- LeetCode/141_linked_list_cycle.cpp
+++ LeetCode/141_linked_list_cycle.cpp
@@ -31,3 +31,33 @@ public:
         return false;
     }
 };
+
+// Method 2 fast slow pointers
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast && fast->next!=NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+
+            if(fast->next == slow)
+            {
+                return true;
+            }
+                        fast = fast->next;
+        }
+        return false;
+        
+    }
+};
