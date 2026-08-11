6f32c546a437ebe9d4c724fd6741f6993ab05928
Adivya Jain <adivyajain2003@gmail.com>
2026-08-12 01:08:19 +0530

    striver

--- LeetCode/121_buyAndSellStock.cpp
+++ LeetCode/121_buyAndSellStock.cpp
@@ -12,3 +12,20 @@ public:
         return profit;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //optimal
+        int maxi = 0;
+        int n = prices.size();
+        vector<int> maxiarr(n,0);
+        maxiarr[n-1] = prices[n-1];
+        for (int i = n - 2 ; i >=0 ; i--) {
+            maxiarr[i] = max(maxiarr[i+1],prices[i]);
+            maxi = max(maxi,maxiarr[i] - prices[i]);
+        }
+
+        return maxi;
+    }
+};
