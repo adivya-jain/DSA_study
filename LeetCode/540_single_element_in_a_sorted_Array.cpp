dd02cfd8e521064bad8e1cc32d409b8d65a314fb
Adivya Jain <adivyajain2003@gmail.com>
2026-08-21 16:47:45 +0530

    murli sir

---
+++ LeetCode/540_single_element_in_a_sorted_Array.cpp
@@ -0,0 +1,31 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int ans = 0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             ans = ans ^ nums[i];
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid = mid - 1;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
