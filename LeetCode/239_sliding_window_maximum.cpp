bd61e224868fe39948af658e20d0a741f1b0c28f
Adivya Jain <adivyajain2003@gmail.com>
2026-09-16 01:11:36 +0530

    DEQUE VVIMP striver

---
+++ LeetCode/239_sliding_window_maximum.cpp
@@ -0,0 +1,31 @@
+// DEQUE
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> ans;
+        // first window
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+
+        ans.push_back(nums[dq.front()]);
+
+        for (int i = k; i < nums.size(); i++) {
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
